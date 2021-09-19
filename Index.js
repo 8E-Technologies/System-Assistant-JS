503fb80fd6dd5874e90db19529427fda26afd2a0
Brian Leyva <55462869+xxSShadow@users.noreply.github.com>
2021-09-19 15:55:06 -0700

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,29 @@
+Discord = require('discord.js');
+FileSync = require('fs');
+Client = new Discord.Client();
+
+Prefix = process.env['PREFIX'];
+Token = process.env['TOKEN'];
+
+Client.commands = new Discord.Collection();
+Folder = FileSync.readdirSync('./Commands/');
+Folder.filter(File => File.endsWith('.js'));
+
+for (File of Folder) {
+  Command = require(`./Commands/${File}`);
+  Client.commands.set(Command.name, Command);
+}
+
+Client.once('ready', async () => {
+  console.log(`${Client.user.username} Is Online!`);
+});
+
+Client.on('message', async (message) => {
+  if (!message.content.startsWith(Prefix) || message.author.bot) {return}
+  Arguments = message.content.slice(Prefix.length).split(/ +/);
+  Command = Arguments.shift();
+  
+  if (Command === 'help') {Client.commands.get('help').execute(message)}
+});
+
+Client.login(Token);
