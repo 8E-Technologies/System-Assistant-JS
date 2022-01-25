56ac7cc9bced240adb4d39f2941436f06b3d28cf
RealGingerMcFerren <86018761+RealGingerMcFerren@users.noreply.github.com>
2022-01-24 22:12:06 -0500

    Removed Sensitive Info
    Code is about to become public. Removed Token hidden in code and my Legal Name, replacing it with Ginger.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 Discord = require('discord.js');
 Bot = new Discord.Client();
-Prefix = ("PREFIX 1");
+Prefix = ("Hey System");
 Prefix2 = ('PREFIX 2');
 Token = ('TOKEN');
 Operations = ("Yes");
-Admin1 = ("Troy D. Moon");
+Admin1 = ("Ginger McFerren");
 Admin2 = ("xxSShadow");
-Device = ("Troy's 8E Laptop (Visual Studio)");
+Device = ("AL Server");
 Status = ("Online");
 Version = ("5.0.0");
 
@@ -96,92 +96,6 @@ Bot.on('message', msg => {
     if (msg.content === `${Prefix}Ping.`) {
         msg.author.send(`:ping_pong: **Ping**\nThis Device is: Troy's Laptop.\nLatency: ${Date.now() - msg.createdTimestamp}\nAPI Delay: ${Math.round(Bot.ws.ping)}\nJitter: N/A \nHeartbeat Ping: N/A`)
         console.info(`Ping.`);
-    }
-
-    //Development Commands
-    //These Commands are logged.
-
-    if (msg.content == `${Prefix2}Info.`) {
-        msg.author.send(`**DEV INFO FOR SYSTEM ASSISTANT BOT**\nToken:'NzkxMDc3NzkwNDIxMzUyNDY4.X-J6mg.QVZEE8easKV-gIZiKagrFlaRd6Y'\nBuild: 9753 (19612.736)\nID: 791077790421352468`)
-        console.info(`Info. +Dev`);
-    }
-
-    if (msg.content == `${Prefix2}Play Among Us`) {
-        Bot.user.setActivity("Among Us");
-        msg.authot.send(`Set.`)
-        console.info(`Play Among Us +Dev`);
-    }
-
-    if (msg.content == `${Prefix2}Activity Help.`) {
-        msg.author.send(`**Available Activity Command**\n${Prefix2}Play Among Us\n${Prefix2}Play my laptop\n${Prefix2}Play my phone\n${Prefix2}Play with friends\n${Prefix2}Play clear\n${Prefix2}Stream among us 1\n${Prefix2}Stream among us 2\n${Prefix2}Stream among us 3\n${Prefix2}Watch Linus Tech Tips\n${Prefix2}Listen to Emergency Alerts`)
-        console.info(`Activity Help. +Dev`);
-    }
-
-    if (msg.content == `${Prefix2}Help.`) {
-        msg.author.send(`**8E DEVELOPER HELP**\n${Prefix2}Activity Help.: Shows the available Bot Status commands.\n${Prefix2}Crash.: Kills the bot.\n${Prefix2}Backup Crash.: Crashes the bot in case normal crash doesn't work.\n${Prefix2}Info.: Grabs Development Info.`)
-        console.info(`Help. +Dev`);
-    }
-
-    if (msg.content == `${Prefix2}Crash.`) {
-        die();
-    }
-
-    if (msg.content == `${Prefix2}Backup Crash.`) {
-        msg.author.send(`Crashing...`)
-        msg.channel.send(crash)
-    }
-
-    if (msg.content == `${Prefix2}Play my laptop`) {
-        Bot.user.setActivity("on Troy's 8E Laptop");
-        msg.author.send(`Set.`)
-        console.info(`Play my laptop +Dev`);
-    }
-
-    if (msg.content == `${Prefix2}Play my phone`) {
-        Bot.user.setActivity("on Troy's 8E Phone");
-        msg.author.send(`Set.`)
-        console.info(`Play my phone +Dev`);
-    }
-
-    if (msg.content == `${Prefix2}Play with friends`) {
-        Bot.user.setActivity("with friends");
-        msg.author.send(`Set.`)
-        console.info(`Play with friends +Dev`);
-    }
-
-    if (msg.content == `${Prefix2}Play clear`) { Bot.user.setActivity(null);
-        msg.author.send(`Cleared`)
-        console.info(`Play clear +Dev`);
-    }
-
-    if (msg.content == `${Prefix2}Stream among us 1`) {
-        Bot.user.setActivity("Among Us", { type: "STREAMING", url: "https://twitch.tv/xxSSShadow" });
-        msg.author.send(`Set.`)
-        console.info(`Stream among us 1 +Dev`);
-    }
-
-    if (msg.content == `${Prefix2}Stream among us 2`) {
-        Bot.user.setActivity("Among Us", { type: "STREAMING", url: "https://twitch.tv/troymoondiscord" });
-        msg.author.send(`Set.`)
-        console.info(`Stream among us 2 +Dev`);
-    }
-
-    if (msg.content == `${Prefix2}Stream among us 3`) {
-        Bot.user.setActivity("Among Us", { type: "STREAMING", url: "https://twitch.tv/manny_da_fast21" });
-        msg.author.send(`Set.`)
-        console.info(`Stream among us 3 +Dev`);
-    }
-
-    if (msg.content == `${Prefix2}Watch Linus Tech Tips`) {
-        Bot.user.setActivity("Linus Tech Tips", { type: "WATCHING" });
-        msg.author.send(`Set.`)
-        console.info(`Watch Linus Tech Tips +Dev`);
-    }
-
-    if (msg.content == `${Prefix2}Listen to Emergency Alerts`) {
-        Bot.user.setActivity("Emergency Alerts", { type: "LISTENING" });
-        msg.author.send(`Set.`)
-        console.info(`Listen to Emergency Alerts +Dev`);
     }
 
 });
